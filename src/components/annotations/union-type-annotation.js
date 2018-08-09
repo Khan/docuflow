bdcf3bd29dcec2d785ccce92fc1ed64aab62d943
Kevin Barabash <kevinb@khanacademy.org>
2018-08-09 10:14:34 -0400

    style union types correctly

--- src/components/annotations/union-type-annotation.js
+++ src/components/annotations/union-type-annotation.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from "react";
+import {StyleSheet, css} from "aphrodite";
 
 import TypeAnnotation from "./type-annotation.js";
 
@@ -14,8 +15,15 @@ export default class UnionTypeAnnotation extends React.Component<Props> {
     render() {
         const {node} = this.props;
         const types = node.types.map(t => <TypeAnnotation node={t} />);
-        return <span>
+        return <span className={css(styles.container)} >
             {intersperse(types, <span> | </span>)}
         </span>
     }
 }
+
+const styles = StyleSheet.create({
+    container: {        
+        fontFamily: "monospace",
+        fontSize: 16,
+    },
+});
